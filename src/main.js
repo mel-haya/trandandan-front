867d003e7214d50af8c94925372721f9e7bd1167
Mourad <yas20072003@gmail.com>
2022-08-14 13:39:00 +0100

    trying to remove background pff

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+var script = require('./p5script.js')
+const P5 = require('p5')
 
 createApp(App).mount('#app')
+new P5(script.main, document.getElementById("p5Canvas"))
