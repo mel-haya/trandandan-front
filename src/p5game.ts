956755ae0e6965e6a1b3fc832e035d5fe6efd994
hoummadi <hamza.hoummadi.genie.info@gmail.com>
2022-10-20 18:11:40 +0100

    watch need to be tested, modern done

--- src/p5game.ts
+++ src/p5game.ts
@@ -7,6 +7,7 @@ const s = (p:any):any => {
   let imgwon:any;
   let imglost:any;
   let imgload:any;
+  let imgwait:any;
   if (window.innerWidth > 1400)
     windw = window.innerWidth - (window.innerWidth - 1400) - 100
   else
@@ -25,11 +26,11 @@ const s = (p:any):any => {
   let originrightmouse = 0;
   // console.log(windh)
 
-  function getRandomdy() {
-    let value:number = (Math.random() * 100000) % 24 - 12;
-    value =  (value < 1 && value > -1) ? 2: value;
-    return value;
-  }
+  // function getRandomdy() {
+  //   let value:number = (Math.random() * 100000) % 24 - 12;
+  //   value =  (value < 1 && value > -1) ? 2: value;
+  //   return value;
+  // }
   
   // console.log(getRandomdy());
 
@@ -37,8 +38,8 @@ const s = (p:any):any => {
     x:windw/2,
     y:windh/2,
     r:15,
-    dx:8,
-    dy:getRandomdy(),
+    dx:0,
+    dy:0,
     p1:0,
     p2:0,
     score1:0,
@@ -46,19 +47,20 @@ const s = (p:any):any => {
     playerleft:"",
     playerright:"",
     middleY:0,
+    counter:0,
+    mDY:0,
   }
 
-  console.log(ball.dy);
+  // console.log(ball.dy);
   
-  const DEFAULT  = {w:windh, oldw:oldwindw, mousepos:0, room:"", pos:0}
-  const BALL = {room:"", ped1:0, ped2:0, ball_data:ball}
+  const DEFAULT  = {mousepos:0, room:"", pos:0}
+  const BALL = {ball_data:ball}
 
-  let my_pos:number = 0;
-  let my_room:string = "";
   
   const gameEnded = false;
   let gameStart = false;
   let isLoad = true;
+  let isWait = false;
   let isWon = false;
   let isLost = false;
   let isModern = false;
@@ -81,24 +83,25 @@ const s = (p:any):any => {
     data;
     p.remove();
     iio.off('connection');
-    iio.off('take_pos');
+    iio.off('takePosition');
     iio.off('mouse');
     iio.off('ball');
     iio.off('reset');
     iio.off('restart');
     iio.off('fin');
-    iio.off('done');
+    // iio.off('done');
     iio.off('won');
     iio.off('lost');
+    iio.off('watch_wait');
+    iio.off('watch_work');
+    iio.off('watch_modern');
     // iio.off('testinter');
     iio.disconnect();
   });
   
-  iio.emit("connection", ball);
+  iio.emit("connection", "");
 
   iio.on('connection', (data) => {
-    //iio.emit("update_mouse", {...DEFAULT,oldw:0})
-    console.log(data)
     if (data === "wait")
     {
       isLoad = true;
@@ -121,13 +124,10 @@ const s = (p:any):any => {
     }
   });
 
-  iio.on('take_pos', (data) => {
-    my_pos = data.yourplace;
-    my_room = data.room;
-    DEFAULT.pos = my_pos;
-    DEFAULT.room = my_room;
-    BALL.room = my_room;
-    console.log(my_pos + " " + my_room);
+  iio.on('takePosition', (data) => {
+    DEFAULT.pos = data.yourplace;
+    DEFAULT.room = data.room;
+    console.log(data.yourplace + " " + data.room);
   });
 
   iio.on('mouse', (data) => {
@@ -144,11 +144,13 @@ const s = (p:any):any => {
   });
 
   iio.on('ball', (data) => {
+    // console.log(isModern);
     BALL.ball_data.x = data.x * variationw;
     BALL.ball_data.y = data.y * variationh;
     BALL.ball_data.dx = data.dx;
     BALL.ball_data.dy = data.dy;
-    // console.log("hiiiiiiiii");
+    if (isModern == true)
+      BALL.ball_data.middleY = data.middleY * variationh;
   });
 
   iio.on('reset', (data) => {
@@ -170,16 +172,16 @@ const s = (p:any):any => {
       p.loop();
   });
   
-  iio.on('done', (data) => {
-    p.noLoop();
-    console.log(data);
-    console.log("you win");
-    gameStart = false;
-  });
+  // iio.on('done', (data) => {
+  //   p.noLoop();
+  //   console.log(data);
+  //   console.log("you win");
+  //   gameStart = false;
+  // });
   
   iio.on('won', (data) => {
     data;
-    iio.emit('gamedone', data);
+    //iio.emit('gamedone', data);
     if (isLost === false)
     {
       isWon = true;
@@ -192,6 +194,27 @@ const s = (p:any):any => {
     data;
     isLost = true;
     gameStart = false;
+    p.loop();
+  });
+
+  iio.on('watch_wait', (data) => {
+    data;
+    isLoad = false;
+    isWait = true;
+    p.loop();
+  });
+  
+  iio.on('watch_work', (data) => {
+    data;
+    isLoad = false;
+    isWait = false;
+    gameStart = true;
+    p.loop();
+  });
+
+  iio.on('watch_modern', (data) => {
+    data;
+    isModern = true;
     p.loop();
   });
 
@@ -232,6 +255,7 @@ const s = (p:any):any => {
     function preload() {
       imgwon = p.loadImage('https://media.istockphoto.com/vectors/pixel-art-8bit-you-win-text-with-three-winner-golden-cups-on-black-vector-id1268272329?k=20&m=1268272329&s=170667a&w=0&h=79fO42ChPzO8gcIdzngCuag6_Y9ef2dUh1LWpaOkyXo=');
       imgload = p.loadImage(require('@/assets/waiting.jpg'));
+      imgwait = p.loadImage(require('@/assets/game_wait.jpg'));
       imglost = p.loadImage('https://elements-video-cover-images-0.imgix.net/files/961bcd50-cb8b-4632-b46c-3abb528c984d/inline_image_preview.jpg?auto=compress&crop=edges&fit=crop&fm=jpeg&h=800&w=1200&s=90f07628b0c40410c1e819e2da97c2b8');
     }
 
@@ -260,7 +284,6 @@ const s = (p:any):any => {
       p.clear();
       p.strokeWeight(windh / 100);
       DEFAULT.mousepos = p.mouseY / variationh;
-      DEFAULT.w = windw;
       iio.emit("update_mouse", DEFAULT);
 
       if (window.innerWidth > 1400)
@@ -310,15 +333,19 @@ const s = (p:any):any => {
 
       // to check
       if (isModern == true)
-        p.rect(windw / 2 - 3, BALL.ball_data.middleY, 6, RightPaddleHeight - 10);
+      {
+        p.fill(236,132,236);
+        p.rect(windw / 2 - 3, BALL.ball_data.middleY, 6, windw / 9);
+      }
 
-      BALL.ped1 = originleftmouse - 41;
-      BALL.ped2 = originrightmouse - 41;
 
-      
+      // BALL.ped1 = originleftmouse - 41;
+      // BALL.ped2 = originrightmouse - 41;
+
+      p.fill(250,250 - ((Math.abs(ball.dx) - 7) * 15),250 - ((Math.abs(ball.dx) - 7) * 12));
       if (gameStart == true)
         p.ellipse(BALL.ball_data.x, BALL.ball_data.y, BALL.ball_data.r, BALL.ball_data.r);
-      
+      p.fill(250,250,250);
       
 
       if (oldwindw != windw)
@@ -347,6 +374,11 @@ const s = (p:any):any => {
      
       p.cursor(p.CROSS);
       // p.noCursor();
+      if (isWait == true)
+      {
+        p.imageMode(p.CENTER);
+        p.image(imgwait, windw / 2, windh / 2, (windw * 3) / (4 * 2), (windw * 2) / (4 * 2));
+      }
       if (isLoad == true)
       {
         //console.log("loading");
